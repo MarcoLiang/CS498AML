8dd7ba8605e5a1f32e9371ed5bc9e05acee8acae
Marco Liang <marcoliangbing@gmail.com>
2018-04-29 17:51:26 -0500

    add draft

--- HWs/HW8/ImageDenoising.py
+++ HWs/HW8/ImageDenoising.py
@@ -18,17 +18,25 @@ class ImageDenoising:
         self.Q_init = None
         self.theta_HH = 0.8
         self.theta_HX = 0.2
+        self.energy = np.zeros((20, 10))
+        self.energy_samples = None
+
+        self.load_data()
+        self.preprocessing()
+        self.add_noise()
 
     def preprocessing(self):
         '''
         :return: return write-black photo
         '''
+        print("Preprocessing...")
         idx = self.train_images < 128
         self.train_images = np.ones(self.train_images.shape)
         self.train_images[idx] = -1
         self.noise_images = np.array(self.train_images)
 
     def add_noise(self):
+        print("Adding noise...")
         nclos = 15
         flip_coo = pd.read_csv(self.data_dir + '/NoiseCoordinates.csv', sep=',', usecols=range(1, nclos + 1)).as_matrix()
         flip_coo.astype(np.uint8)
@@ -43,6 +51,7 @@ class ImageDenoising:
         return diff
 
     def load_data(self):
+        print("Loading data...")
         # Load Update Order Coordinate
         nclos = 784
         order_coo = pd.read_csv(self.data_dir + '/UpdateOrderCoordinates.csv', sep=',', usecols=range(1, nclos + 1)).as_matrix()
@@ -54,17 +63,15 @@ class ImageDenoising:
         self.Q_init = pd.read_csv(self.data_dir + '/InitialParametersModel.csv', sep=',', dtype=float, header=None).as_matrix()
         # print(Q[0][0])
 
-    def mean_field_inference(self, iters=10):
-        pass
+        self.energy_samples = pd.read_csv(self.data_dir + '/EnergySamples.csv', sep=',', dtype=float, header=None).as_matrix()
 
     def update(self, iters=10):
-        for iter in range(iters):
-            for i in range(0, self.nimgs * 2 - 1, 2):
+        for i in range(0, self.nimgs * 2 - 1, 2):
+            Q = np.array(self.Q_init)
+            for iter in range(iters):
                 img_idx = i // 2
-                img = self.train_images[img_idx]
-                Q = np.array(self.Q_init)
-                for row, col in zip(self.update_order_coo[i], self.update_order_coo[i + 1]):
-                    self.Q
+                self.update_one_img(Q, img_idx)
+                self.energy[img_idx, iter] = self.variational_free_energy(Q, img_idx)
 
     def update_one_img(self, Q, img_idx):
         coo_row = img_idx * 2
@@ -72,17 +79,46 @@ class ImageDenoising:
         pow = 0
         for row, col in zip(self.update_order_coo[coo_row], self.update_order_coo[coo_col]):
             # Hidden Neighbors
-            for d_row in range(-1, 2, 1):
-                for d_col in range(-1, 2, 1):
-                    n_row = row + d_row
-                    n_col = col + d_col
-                    if n_row >= 0 and n_row < self.img_shape[0] and n_col >= 0 and n_col < self.img_shape[1]:
-                        pow += self.theta_HH * (2 * Q[n_row, n_col] - 1)
+            neighbor_idx = self.neighbors(row, col) # a matrix with shape (n, 2)
+            pow += np.sum(self.theta_HH * (2 * Q[neighbor_idx[:, 0], neighbor_idx[:, 1]] - 1))
             # Observed Neighbors
             pow += self.theta_HX * self.noise_images[img_idx][row, col]
             temp = np.exp(pow)
             pi = temp / (temp + 1 / temp)
             Q[row][col] = pi
+
+    def variational_free_energy(self, Q, img_idx):
+        eps = 10**-10
+        term1 = np.sum(Q * np.log(Q + eps)) + np.sum((1 - Q) * np.log((1 - Q) + eps))
+        # Hidden Neighbors
+        E_q = 2 * Q - 1 # E_q Energy
+        hidden = 0
+        for row in range(self.img_shape[0]):
+            for col in range(self.img_shape[1]):
+                neighbor_idx = self.neighbors(row, col)
+                hidden += self.theta_HH * E_q[row, col] * np.sum(E_q[neighbor_idx[:, 0], neighbor_idx[:, 1]])
+        observed = np.sum(self.theta_HX * E_q * self.noise_images[img_idx])
+        term2 = hidden + observed
+        return term1 + term2
+
+
+    def neighbors(self, row, col):
+        '''
+        :param row: the row of a pixel
+        :param col: the col of a pixel
+        :return: a 2d matrix with shape (n, 2)
+                 where n is number of neighbors of a pixel
+                 and each row is the row and col of its neighbors
+        '''
+        neighbors = []
+        for d_row in range(-1, 2, 1):
+            for d_col in range(-1, 2, 1):
+                n_row = row + d_row
+                n_col = col + d_col
+                if (d_row | d_col) != 0 and n_row >= 0 and n_row < self.img_shape[0] and n_col >= 0 and n_col < self.img_shape[1]:
+                    neighbors.append([n_row, n_col])
+        return np.array(neighbors)
+
 
 
 
@@ -96,19 +132,20 @@ def main():
     data_dir = "SupplementaryAndSampleData"
 
     BM = ImageDenoising(data_dir)
-    BM.preprocessing()
-    BM.add_noise()
-    res = BM.test_add_noise()
+    BM.update(iters=10)
+    print(BM.energy)
+    # res = BM.test_add_noise()
     # print(res[1:])
 
+    # print(BM.variational_free_energy(BM.Q_init, 0))
 
-    imgs = BM.train_images
+
+    # imgs = BM.train_images
     # plt.imshow(res[0], cmap='gray')
     # plt.show()
     # print(imgs[0])
     # scipy.misc.toimage(scipy.misc.imresize(imgs[0, :, :] * -1 + 256, 10.))
 
-    BM.load_data()
 
 if __name__ == "__main__":
     main()
--- HWs/HW8/temp.py
+++ HWs/HW8/temp.py
@@ -4,7 +4,7 @@ import numpy as np
 x = np.array([1, 3, 2, 4, 2])
 y = np.array([2, 4, 3, 1, 3])
 
-# a = np.arange(0, 36).reshape((6, 6))
+a = np.arange(0, 36).reshape((6, 6))
 # print(a)
 # print(a[x, y])
 
@@ -13,6 +13,32 @@ y = np.array([2, 4, 3, 1, 3])
 #     print(j)
 #     print('===')
 
-for dx in range(-1, 2, 1):
-    for dy in range(-1, 2, 1):
-        print("{0}:{1}".format(dx, dy))
+# for dx in range(-1, 2, 1):
+#     for dy in range(-1, 2, 1):
+#         print("{0}:{1}".format(dx, dy))
+
+
+def neighbors(row, col):
+    '''
+    :param row: the row of a pixel
+    :param col: the col of a pixel
+    :return: a 2d matrix with shape (n, 2)
+             where n is number of neighbors of a pixel
+             and each row is the row and col of its neighbors
+    '''
+    neighbors = []
+    for d_row in range(-1, 2, 1):
+        for d_col in range(-1, 2, 1):
+            n_row = row + d_row
+            n_col = col + d_col
+            if (d_row | d_col) != 0 and n_row >= 0 and n_row < 6 and n_col >= 0 and n_col < 6:
+                neighbors.append([n_row, n_col])
+    return np.array(neighbors)
+
+# print(neighbors(1,1))
+print(a)
+nei = neighbors(0, 1)
+# print(nei)
+
+print(a[nei[:, 0], nei[:, 1]])
+
